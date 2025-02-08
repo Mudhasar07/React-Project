65cb30355dcd85bda1b534f9e932745542d155af
Ahamed Althaf <ahamedalthaf@Ahameds-MacBook-Pro.local>
2025-02-08 16:52:33 +0530

    reactCreateElement-JsxElement-JsxTranspilation

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
-// const heading = React.createElement("h1", {
-//     id:"heading1", xyz:"abc", name:"seeni"
-// }, "Hello World From React!!");
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(heading);
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("div", {id: "heading"}, [
-    React.createElement("div", {id:"child1"}, [
-                React.createElement("h1", {}, "I am from D1h1"),
-                React.createElement("h2", {}, "I am from D1h2"),
-        ]),
-        React.createElement('div', { id:"child2" },[
-            React.createElement('h1', {}, "I am from D2h2"),
-            React.createElement('h2', {}, "I am from D2h2")
-        ])
-    ]);
+// Creating React using Normal React.CreateElement
+
+// const heading = React.createElement("h1", {id: "heading"}, "Hello From Normal React CreateElement");
+
+//  // Creating React Element using JSX -> Javascript Extension:
+
+const JsxHeading = (
+  <h1 id="heading" className="heading">
+    React Element Using JSX Syntax
+  </h1>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+// root.render(heading);
+
+// console.log(heading, "Normal React Ceatte Element");
+
+root.render(JsxHeading);
