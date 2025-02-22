1e7e5c65ae832471cb101c860f565f825943367f
Ahamed Althaf <ahamedalthaf@Ahameds-MacBook-Pro.local>
2025-02-22 07:54:40 +0530

    Nested-ReactElement-Created-Using-ArraySymbol

--- app.js
+++ app.js
@@ -1,7 +1,39 @@
-const heading = React.createElement("h1", {id: "heading", xyz: "abc"}, "I am from React!");
+/**
+ * 
+ * <div id="parent">
+ *      <div id="child">
+ *          <h1> heading 1 </h1>
+ *          <h2> heading 2 </h2>
+ *      </div>
+ *          
+ *      <div id="child2">
+ *          <h1> heading 1 </h1>
+ *          <h2> heading 2 </h2>
+ *      </div>
+ * </div>
+ * 
+ */
 
-console.log(heading, "headingTag");
+// To add sibling -> we have to wrap inside ->  []
 
-const findingTheRoot = ReactDOM.createRoot(document.getElementById("root"));
+const heading = React.createElement("div", {id:"parent"}, 
+    [
+        React.createElement("div", {id:"child"}, 
+            [
+                React.createElement("h1", {}, "I am heading 1"),
+                React.createElement("h2", {}, "I am heading 2")
+            ]
+        ),
+        React.createElement("div", {id:"child2"}, 
+            [
+                React.createElement("h1", {}, "I am heading 1"),
+                React.createElement("h2", {}, "I am heading 2")
+            ]
+        )
+    ]
+    
+)
 
-findingTheRoot.render(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
