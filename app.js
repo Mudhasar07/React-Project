777221e81c8ae24d2abf66b7af87a8c89ad127fc
Ahamed Althaf <ahamedalthaf@Ahameds-MacBook-Pro.local>
2025-02-22 19:56:42 +0530

    JSX-BabelTranspile-JxtMultiLine-Syntax

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 
-const heading = React.createElement("div", {id:"parent"}, 
-    [
-        React.createElement("div", {id:"child"}, 
-            [
-                React.createElement("h1", {}, "I am heading 1"),
-                React.createElement("h2", {}, "I am heading 2")
-            ]
-        ),
-        React.createElement("div", {id:"child2"}, 
-            [
-                React.createElement("h1", {}, "I am heading 1"),
-                React.createElement("h2", {}, "I am heading 2")
-            ]
-        )
-    ]
-    
-)
+const heading = React.createElement("h1", {id: "heading"}, "The heading 1"); // Normal create Element
+
+/**
+ * 
+ * 
+ * JSX is a not pure JS Code. JS Browser does not understand JSX. its throw error.
+ * 
+ * Babel will Convert JSX into normal React.createElement.
+ * 
+ * JSX => Babel transpile it to React.createElement => ReactElement as (JS object) => change to HTML Element => RENDER it to browser.
+ * 
+ * 
+ */
+
+const jsxHeading = (<h1 id="heading" >
+    Heading from JSX - Babel
+    </h1>) // JSX syntax for creating element
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(jsxHeading);
